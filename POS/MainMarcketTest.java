3f128d9e3117e3d40052bb1a88cb3cd6860f341d
kdm0904 <45680681+kdm0904@users.noreply.github.com>
2019-10-18 18:58:03 +0900

    Add files via upload

---
+++ POS/MainMarcketTest.java
@@ -0,0 +1,11 @@
+package MarketPos;
+
+public class MainMarcketTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MarkmanagementProgram mainProgram = new MarkmanagementProgram();
+		mainProgram.go();
+	}
+
+}
